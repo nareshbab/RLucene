5c0aa4bfb4328e9e0ac9991a80354b45f9061060
Naresh <naresh.kumar@mu-sigma.com>
2014-04-25 10:03:09 +0000

    Added indexing method

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onLoad <- function(libname, pkgname) {
   .jpackage(pkgname, lib.loc=libname)
-   path <- .jarray(c("/vagrant/work/nanda_indexes/","/vagrant/work/try3/"))
+   path <- .jarray(c("<path to load indexes to RAM>","<path to create indexes>"))
   .session$lo <- .jnew("Lucene", path)
 }
