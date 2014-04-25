fda1391d9704d87a9c2f366875746266ab8fd2e6
Naresh <naresh.kumar@mu-sigma.com>
2014-04-25 19:14:42 +0000

    Fast Highlighting

--- inst/java/Lucene.java
+++ inst/java/Lucene.java
@@ -25,6 +25,7 @@ import org.apache.lucene.search.vectorhighlight.SimpleFragListBuilder;
 import org.apache.lucene.search.vectorhighlight.SimpleFragmentsBuilder;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.store.MMapDirectory;
 import org.apache.lucene.store.RAMDirectory;
 import org.apache.lucene.util.Version;
 import org.json.JSONObject;
@@ -40,20 +41,26 @@ public class Lucene {
 	public IndexWriter writer;
 	public FragListBuilder fragListBuilder;
 	public FragmentsBuilder fragmentsBuilder;
+	public FastVectorHighlighter highlighter;
 	public IndexWriterConfig config;
 	public IndexSearcher searcher;
 	String[] fields = {"notebook_id","description","created_at","updated_at","content","starcount","avatar_url","user_url","commited_at","user"};
 	public static String[] pretag = {"<b style='background:yellow'>"}; 
 	public static String[] posttag = {"</b>"}; 
 	public MultiFieldQueryParser multiparser;
-	
+	boolean unmapHack = true;
 	public static void main (String[] args) throws Exception {
 	}
 
 	public Lucene(String[] path) throws IOException{ 
 		version = Version.LUCENE_47;
 		SA = new StandardAnalyzer(version);
-		dirIndex = FSDirectory.open(new File(path[0]));
+		MMapDirectory index = new MMapDirectory(new File(path[0]));
+		try {
+      			index.setUseUnmap(unmapHack);
+		 } catch (Exception e) {
+      			System.out.println("Unmap not supported on this JVM, continuing on without setting unmap\n" + e);
+    		}
 		writeIndex = FSDirectory.open(new File(path[1]));
 		reader = DirectoryReader.open(dirIndex);
 		searcher = new IndexSearcher(reader);
@@ -61,19 +68,20 @@ public class Lucene {
 		writer = new IndexWriter(writeIndex, config);
 		multiparser = new MultiFieldQueryParser(version, fields, SA);
 		fragListBuilder = new SimpleFragListBuilder();
-		fragmentsBuilder = new SimpleFragmentsBuilder(pretag, posttag);
+		fragmentsBuilder = new SimpleFragmentsBuilder(pretag, posttag);	
+		highlighter = new FastVectorHighlighter(true, true, fragListBuilder, fragmentsBuilder);
 		
 	}
+	
 	public String getResults(String query) throws Exception{
 		Query q = multiparser.parse(query);
 		TopDocs hits = searcher.search(q,1000);
-		FastVectorHighlighter highlighter = new FastVectorHighlighter(true, true, fragListBuilder, fragmentsBuilder);
 		FieldQuery fq = highlighter.getFieldQuery(q);
 		JSONObject json = new JSONObject();
 		for (ScoreDoc sd : hits.scoreDocs) {
         	Document doc = searcher.doc(sd.doc);
 			String frag = highlighter.getBestFragment(fq, reader, sd.doc, "content", 2000000000);
-			json.put(doc.get("notebook_id"), frag);
+			json.put(doc.get("id"), frag);
         }
 		return json.toString();
 	}
