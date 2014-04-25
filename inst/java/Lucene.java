f75d1ebd80f1aa4194ed48e94bd85210427e8b07
Naresh <naresh.kumar@mu-sigma.com>
2014-04-25 19:08:44 +0000

    Fast Highlighting

--- inst/java/Lucene.java
+++ inst/java/Lucene.java
@@ -1,13 +1,13 @@
+
 import java.io.File;
 import java.io.IOException;
-import org.apache.lucene.document.TextField;
 import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
+import org.apache.lucene.document.Field.Index;
 import org.apache.lucene.document.Field.Store;
-import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.Field.TermVector;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
@@ -17,21 +17,19 @@ import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopDocs;
-import org.apache.lucene.search.highlight.Formatter;
-import org.apache.lucene.search.highlight.Fragmenter;
-import org.apache.lucene.search.highlight.Highlighter;
-import org.apache.lucene.search.highlight.QueryScorer;
-import org.apache.lucene.search.highlight.SimpleHTMLFormatter;
-import org.apache.lucene.search.highlight.SimpleSpanFragmenter;
-import org.apache.lucene.search.highlight.TextFragment;
-import org.apache.lucene.search.highlight.TokenSources;
+import org.apache.lucene.search.vectorhighlight.FastVectorHighlighter;
+import org.apache.lucene.search.vectorhighlight.FieldQuery;
+import org.apache.lucene.search.vectorhighlight.FragListBuilder;
+import org.apache.lucene.search.vectorhighlight.FragmentsBuilder;
+import org.apache.lucene.search.vectorhighlight.SimpleFragListBuilder;
+import org.apache.lucene.search.vectorhighlight.SimpleFragmentsBuilder;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
-import org.apache.lucene.store.IOContext;
 import org.apache.lucene.store.RAMDirectory;
 import org.apache.lucene.util.Version;
 import org.json.JSONObject;
 
+@SuppressWarnings("deprecation")
 public class Lucene {
 	public Analyzer SA;
 	public Version version;
@@ -40,68 +38,59 @@ public class Lucene {
 	public RAMDirectory index;
 	public IndexReader reader;
 	public IndexWriter writer;
+	public FragListBuilder fragListBuilder;
+	public FragmentsBuilder fragmentsBuilder;
 	public IndexWriterConfig config;
 	public IndexSearcher searcher;
-	public Formatter shtml;
 	String[] fields = {"notebook_id","description","created_at","updated_at","content","starcount","avatar_url","user_url","commited_at","user"};
+	public static String[] pretag = {"<b style='background:yellow'>"}; 
+	public static String[] posttag = {"</b>"}; 
 	public MultiFieldQueryParser multiparser;
-	public static void main (String[] args) throws Exception {
-		
-	}
 	
+	public static void main (String[] args) throws Exception {
+	}
+
 	public Lucene(String[] path) throws IOException{ 
 		version = Version.LUCENE_47;
 		SA = new StandardAnalyzer(version);
 		dirIndex = FSDirectory.open(new File(path[0]));
 		writeIndex = FSDirectory.open(new File(path[1]));
+		reader = DirectoryReader.open(dirIndex);
+		searcher = new IndexSearcher(reader);
 		config = new IndexWriterConfig(version, SA);
 		writer = new IndexWriter(writeIndex, config);
-		index = new RAMDirectory(dirIndex, new IOContext());
-		reader = DirectoryReader.open(index);
-		searcher = new IndexSearcher(reader);
 		multiparser = new MultiFieldQueryParser(version, fields, SA);
-		shtml = new SimpleHTMLFormatter("<b style='background:yellow'>", "</b>");
+		fragListBuilder = new SimpleFragListBuilder();
+		fragmentsBuilder = new SimpleFragmentsBuilder(pretag, posttag);
+		
 	}
 	public String getResults(String query) throws Exception{
 		Query q = multiparser.parse(query);
 		TopDocs hits = searcher.search(q,1000);
-		QueryScorer scorer = new QueryScorer(q, "content");
-        Highlighter highlighter = new Highlighter(shtml, scorer);
-        highlighter.setMaxDocCharsToAnalyze(900000000);
-        Fragmenter fragmenter = new SimpleSpanFragmenter(scorer);
-        highlighter.setTextFragmenter(fragmenter);
-        JSONObject json = new JSONObject();
-        for (ScoreDoc sd : hits.scoreDocs) {
+		FastVectorHighlighter highlighter = new FastVectorHighlighter(true, true, fragListBuilder, fragmentsBuilder);
+		FieldQuery fq = highlighter.getFieldQuery(q);
+		JSONObject json = new JSONObject();
+		for (ScoreDoc sd : hits.scoreDocs) {
         	Document doc = searcher.doc(sd.doc);
-        	String content = doc.get("content");
-            TokenStream stream = TokenSources.getAnyTokenStream(searcher.getIndexReader(), sd.doc, "content", doc, SA);
-            TextFragment[] frag = highlighter.getBestTextFragments(stream, content, true, 10000000);
-            String x=null;
-            if ((frag[0] != null) && (frag[0].getScore() > 0)) {
-                     x = frag[0].toString();
-            }
-            json.put(doc.get("notebook_id"),x);
+			String frag = highlighter.getBestFragment(fq, reader, sd.doc, "content", 2000000000);
+			json.put(doc.get("notebook_id"), frag);
         }
 		return json.toString();
 	}
 	
-	public void indexing(String[] data) throws Exception {
-		addDoc(writer, data);
+	public void indexing(String[] data) throws IOException {
+		Document doc = new Document();
+		doc.add(new Field("notebook_id", data[0], Store.YES, Index.ANALYZED, TermVector.WITH_POSITIONS_OFFSETS));
+		doc.add(new Field("description", data[1], Store.YES, Index.ANALYZED, TermVector.WITH_POSITIONS_OFFSETS));
+		doc.add(new Field("created_at", data[1], Store.YES, Index.ANALYZED, TermVector.WITH_POSITIONS_OFFSETS));
+		doc.add(new Field("updated_at", data[1], Store.YES, Index.ANALYZED, TermVector.WITH_POSITIONS_OFFSETS));
+		doc.add(new Field("content", data[1], Store.YES, Index.ANALYZED, TermVector.WITH_POSITIONS_OFFSETS));
+		doc.add(new Field("starcount", data[1], Store.YES, Index.ANALYZED, TermVector.WITH_POSITIONS_OFFSETS));
+		doc.add(new Field("avatar_url", data[1], Store.YES, Index.ANALYZED, TermVector.WITH_POSITIONS_OFFSETS));
+		doc.add(new Field("user_url", data[1], Store.YES, Index.ANALYZED, TermVector.WITH_POSITIONS_OFFSETS));
+		doc.add(new Field("commited_at", data[1], Store.YES, Index.ANALYZED, TermVector.WITH_POSITIONS_OFFSETS));
+		doc.add(new Field("user", data[1], Store.YES, Index.ANALYZED, TermVector.WITH_POSITIONS_OFFSETS));
+		writer.addDocument(doc);
 		writer.close();
-	}
-	
-	public static void addDoc(IndexWriter writer, String[] data) throws IOException {
-		  Document doc = new Document();
-		  doc.add(new StringField("notebook_id", data[0], Store.YES));
-		  doc.add(new TextField("description", data[1], Store.YES));
-		  doc.add(new TextField("created_at", data[2], Store.YES));
-		  doc.add(new TextField("updated_at", data[3], Store.YES));
-		  doc.add(new TextField("content", data[4], Store.YES));
-		  doc.add(new StringField("starcount", data[5], Store.YES));
-		  doc.add(new TextField("avatar_url", data[6], Store.YES));
-		  doc.add(new TextField("user_url", data[7], Store.YES));
-		  doc.add(new TextField("commited_at", data[8], Store.YES));
-		  doc.add(new TextField("user", data[9], Store.YES));
-		  writer.addDocument(doc);
 	}
 }
